b3479c6c90ca4814242d366c05d6af9e2bd15f89
Matt <sweeney.matt93@gmail.com>
2022-12-23 15:49:49 -0600

    both axios calls are working and rendering data

--- weather_or_not/src/components/Dashboard.jsx
+++ weather_or_not/src/components/Dashboard.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 export default function Weather () {
 
   const [weather, setWeather] = useState(null)
+  const [orNot, setOrNot] = useState(null)
 
 useEffect(()=>{
   const getData = async () =>{
@@ -18,14 +19,14 @@ useEffect(()=>{
 }, [])
 
 useEffect(()=>{
-  const getData = async () =>{
-  const response = await axios.get('https://api.open-meteo.com/v1/forecast?latitude=41.85&longitude=-87.65&daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,rain_sum,snowfall_sum,windspeed_10m_max&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago')
+  const getOrNot = async () =>{
+  const response = await axios.get('http://localhost:8000/user/')
 
-  setWeather(response.data.daily)
-  console.log(response.data.daily)
+  setOrNot(response.data)
+  console.log(response.data[0].name)
   }
 
-  getData()
+  getOrNot()
 
 }, [])
 
@@ -37,6 +38,8 @@ if(!weather) {
     <div className='container'>
 
       <h1>{weather.rain_sum[0]}</h1>
+
+      <h1>{orNot[0].name}</h1>
 
     </div>
   )
