db3f341a893de1789338377f1191a46b06897e22
Matt <sweeney.matt93@gmail.com>
2022-12-23 13:26:44 -0600

    axios call works for third party API

---
+++ weather_or_not/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Dashboard from "./components/Dashboard"
+
+function App() {
+  return (
+    <div>
+    <h1>hello world</h1>
+    <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ weather_or_not/src/components/Dashboard.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
+
+export default function Weather () {
+
+  const [weather, setWeather] = useState(null)
+
+useEffect(()=>{
+  const getData = async () =>{
+  const response = await axios.get('https://api.open-meteo.com/v1/forecast?latitude=41.85&longitude=-87.65&daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,rain_sum,snowfall_sum,windspeed_10m_max&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago')
+
+  setWeather(response.data.daily)
+  console.log(response.data.daily)
+  }
+
+  getData()
+
+}, [])
+
+useEffect(()=>{
+  const getData = async () =>{
+  const response = await axios.get('https://api.open-meteo.com/v1/forecast?latitude=41.85&longitude=-87.65&daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,rain_sum,snowfall_sum,windspeed_10m_max&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FChicago')
+
+  setWeather(response.data.daily)
+  console.log(response.data.daily)
+  }
+
+  getData()
+
+}, [])
+
+
+if(!weather) {
+  return <h2>Loading Dashboard</h2>
+}else{
+  return(
+    <div className='container'>
+
+      <h1>{weather.rain_sum[0]}</h1>
+
+    </div>
+  )
+}}
+
+
