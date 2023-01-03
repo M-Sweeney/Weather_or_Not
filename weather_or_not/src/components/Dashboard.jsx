c0d56d485751e14fd4169e80bcfe14e6e0f8432a
Matt <sweeney.matt93@gmail.com>
2023-01-03 14:04:40 -0600

    condition if statement is written and working

--- weather_or_not/src/components/Dashboard.jsx
+++ weather_or_not/src/components/Dashboard.jsx
@@ -10,7 +10,8 @@ export default function Weather () {
     snow: null,
     wind: null
   })
-  const [orNot, setOrNot] = useState(null)
+
+  const [user, setUser] = useState([])
 
 useEffect(()=>{
   const getData = async () =>{
@@ -23,6 +24,8 @@ useEffect(()=>{
     snow: response.data.daily.snowfall_sum[0],
     wind: response.data.daily.windspeed_10m_max[0]
   })
+
+  console.log(weather)
   }
 
   getData()
@@ -32,29 +35,51 @@ useEffect(()=>{
 
 
 useEffect(()=>{
-  const getOrNot = async () =>{
+  const getUser = async () =>{
   const response = await axios.get('http://localhost:8000/user/')
 
-  setOrNot(response.data)
-  console.log(response.data[0].name)
+  setUser(response.data[0])
+  console.log(response.data[0])
   }
 
-  getOrNot()
+  getUser()
 
 }, [])
 
 
-if(!weather) {
+// if(!user && weather) {
+//   return <h2>Loading Dashboard</h2>
+// }else{
+//   return(
+//     <div className='container'>
+//       <h1>{user.name}</h1>
+//       <h1>{weather.highTemperature}</h1>
+//     </div>
+
+//   )
+// }}
+
+let condition;
+
+if (weather.rain >= 30){
+  condition = `with a ${weather.rain}% chance of rain.`
+} else if (weather.snow >= 30){
+  condition = `with a ${weather.snow}% chance of snow.`
+} else if (weather.wind >= 21){
+  condition = `with windspeeds of ${weather.wind}mph.`
+} else{ condition = "."}
+
+if(!weather && user) {
   return <h2>Loading Dashboard</h2>
 }else{
   return(
-    <div className='container'>
-      {/* <h1>{weather.rain_sum[0]}</h1>
-      <h1>{orNot[0].name}</h1> */}
-      <h1>{weather.highTemperature}</h1>
-    </div>
+    <div className="dashboardContainer">
+    <h1>Good Morning {user.name}!</h1>
 
+    <h2>Today in {user.city} we will have a high temperature of {weather.highTemperature}° and a low of {weather.lowTemperature}° {condition}</h2>
+
+    <h2>Based on those weather conditions I would recommend dressing for the temperature with - and dressing for the conditions with - item- </h2>
+    
+    </div>
   )
 }}
-
-
