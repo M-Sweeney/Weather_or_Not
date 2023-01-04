c8a9d6ea71fe81a0b398fd9123f24765712ee7ba
Matt <sweeney.matt93@gmail.com>
2023-01-04 08:27:39 -0600

    got my temperature based items mapping and displaying.

--- weather_or_not/src/components/Dashboard.jsx
+++ weather_or_not/src/components/Dashboard.jsx
@@ -11,7 +11,7 @@ export default function Weather () {
     wind: null
   })
 
-  const [user, setUser] = useState([])
+  const [user, setUser] = useState(null)
 
 useEffect(()=>{
   const getData = async () =>{
@@ -24,62 +24,138 @@ useEffect(()=>{
     snow: response.data.daily.snowfall_sum[0],
     wind: response.data.daily.windspeed_10m_max[0]
   })
-
-  console.log(weather)
-  }
+  
+}
+// console.log(weather)
 
   getData()
 
 }, [])
 
-
+// let hotItems = null
+// let warmItems = null
+// let coolItems = null
+// let coldItems = null
+// let rainItems = null
+// let snowItems = null
+// let windItems = null
 
 useEffect(()=>{
   const getUser = async () =>{
   const response = await axios.get('http://localhost:8000/user/')
 
   setUser(response.data[0])
-  console.log(response.data[0])
   }
-
   getUser()
+  console.log(user)
+
 
 }, [])
 
+useEffect(()=> {
+  console.log(user)
+// hotItems = user.item.filter(item => item.hot)
+// warmItems = user.item.filter(item => item.warm)
+// coolItems = user.item.filter(item => item.cool)
+// coldItems = user.item.filter(item => item.cold)
+// rainItems = user.item.filter(item => item.rain)
+// snowItems = user.item.filter(item => item.snow)
+// windItems = user.item.filter(item => item.wind)
+}, [user])
 
-// if(!user && weather) {
-//   return <h2>Loading Dashboard</h2>
-// }else{
-//   return(
-//     <div className='container'>
-//       <h1>{user.name}</h1>
-//       <h1>{weather.highTemperature}</h1>
-//     </div>
 
-//   )
-// }}
+function average(num1, num2) {
+  return (num1 + num2) / 2
+}
 
-let condition;
+let averageTemp = average(weather.highTemperature, weather.lowTemperature)
 
-if (weather.rain >= 30){
-  condition = `with a ${weather.rain}% chance of rain.`
-} else if (weather.snow >= 30){
-  condition = `with a ${weather.snow}% chance of snow.`
+let temp;
+
+if(averageTemp > 85){
+  temp = `hot`
+} else if (averageTemp < 85 && averageTemp > 70){
+  temp = `warm`
+} else if (averageTemp < 70 && averageTemp > 50){
+  temp = `cool`
+} else {
+  temp = `cold`
+}
+
+let condition; 
+let conditionReport;
+
+if (weather.rain >= .1){
+  conditionReport = `with ${weather.rain}inch(es) of rain.`
+  condition = `rain`
+} else if (weather.snow >= .1){
+  conditionReport = `with ${weather.snow}inch(es) snow.`
+  condition = `snow`
 } else if (weather.wind >= 21){
-  condition = `with windspeeds of ${weather.wind}mph.`
-} else{ condition = "."}
+  conditionReport = `with windspeeds of ${weather.wind}mph.`
+  condition = `wind`
+} else{ conditionReport = "."}
 
-if(!weather && user) {
+
+
+// let tempItems;
+
+// if (temp == `cold`){
+//   tempItems = hotItems
+// }
+//   else if(temp == "warm"){
+//   tempItems = warmItems
+// } else if(temp == "cool"){
+//   tempItems = coolItems
+// } else{
+//   tempItems = coldItems
+// }
+
+// let conditionItems;
+
+// if (condition == "rain"){
+//   conditionItems = rainItems
+// } else if(condition == "snow"){
+//   conditionItems = snowItems
+// } else if(condition == "wind"){
+//   conditionItems = windItems
+// } else{
+  
+// }
+
+
+if(!weather || !user) {
   return <h2>Loading Dashboard</h2>
 }else{
+  // console.log(user)
+  // console.log(coldItems)
+  // console.log(tempItems)
   return(
     <div className="dashboardContainer">
     <h1>Good Morning {user.name}!</h1>
 
-    <h2>Today in {user.city} we will have a high temperature of {weather.highTemperature}° and a low of {weather.lowTemperature}° {condition}</h2>
+    <h2>Today in {user.city} it will be fairly {temp} with a high of {weather.highTemperature}° and a low of {weather.lowTemperature}° {conditionReport}</h2>
 
-    <h2>Based on those weather conditions I would recommend dressing for the temperature with - and dressing for the conditions with - item- </h2>
-    
+    <h2>Based on those weather conditions I would recommend dressing for the {temp} temperature with - and dressing for the conditions with - item- </h2>
+
+    {user.item.map((item) => {
+      console.log(item[temp])
+      if(item[temp] === true){
+        return(
+        <div>
+          <h3>{item.name}</h3>
+        </div>
+)}})}
+
+{user.item.map((item) => {
+      console.log(item[condition])
+      if(item[condition] === true){
+        return(
+        <div>
+          <h3>{item.name}</h3>
+        </div>
+)}})}
+
     </div>
   )
 }}
